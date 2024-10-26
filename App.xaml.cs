04ecb3c01ee0b38f1149e5ffe5c9ea07e36503b8
Justin Hardin <jnhardin85@gmail.com>
2024-10-26 16:58:48 -0500

    Initial project setup and folder organization

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PodcastApp9000
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PodcastApp9000
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
